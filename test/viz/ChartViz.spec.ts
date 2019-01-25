233a3f88b48642734731ff0ba6cb78d6e85c10fb
Noah <noahbres@gmail.com>
2019-01-25 03:58:32 -0600

    chore(ChartViz): Create a test for ChartViz that doesn't even work
    SVG.js breaks the tests so just ignore this entire file

---
+++ test/viz/ChartViz.spec.ts
@@ -0,0 +1,63 @@
+import Heredity from "../../src/Heredity";
+import ChartViz from "../../src/viz/ChartViz";
+import NumberChromosome from "../../src/chromosomes/NumberChromosome";
+
+describe("Char Viz", () => {
+  let heredity: Heredity;
+  let viz: ChartViz;
+
+  let initSpy: jest.SpyInstance;
+  let updateSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    heredity = new Heredity({
+      populationSize: 10,
+      templateChromosome: new NumberChromosome({}, 5)
+    });
+
+    document.body.innerHTML = `
+      <div id="chart-viz"></div>
+    `;
+
+    initSpy = jest.spyOn(ChartViz.prototype, "init");
+    updateSpy = jest.spyOn(ChartViz.prototype, "update");
+
+    viz = new ChartViz(
+      <HTMLElement>document.getElementById("chart-viz"),
+      heredity
+    );
+  });
+
+  test("Initialize with id", () => {
+    viz = new ChartViz("chart-viz", heredity);
+    expect(document.getElementById("chart-viz-style-id")).not.toBeUndefined();
+  });
+
+  test("Initialize with dom object", () => {
+    expect(document.getElementById("chart-viz-style-id")).not.toBeUndefined();
+  });
+
+  test("Generate Population Hook", () => {
+    heredity.generatePopulation();
+
+    expect(initSpy).toHaveBeenCalled();
+  });
+
+  test("Next Generation Hook", () => {
+    heredity.generatePopulation();
+    heredity.nextGeneration();
+
+    expect(updateSpy).toHaveBeenCalled();
+  });
+
+  test("Link", () => {
+    expect(viz.link(viz)).toBeFalsy();
+  });
+
+  test("Already initialized test", () => {
+    heredity.generatePopulation();
+    heredity.generatePopulation();
+
+    expect(updateSpy).toHaveBeenCalled();
+  });
+});
